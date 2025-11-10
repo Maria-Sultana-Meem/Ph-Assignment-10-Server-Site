e53e312ad01884f49bd6be530d6156dc6d0222fc
Maria-Sultana-Meem <sultanamari639@gmail.com>
2025-11-10 18:15:43 +0600

    firebase adk

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const cors = require('cors')
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./firebase-adminsdk.json");
 require('dotenv').config()
 const app = express()
 const port =process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
+
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x08ux7h.mongodb.net/?appName=Cluster0`;
@@ -18,6 +27,32 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+
+const verifyToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.status(401).send({
+      message: "unauthorized access. Token not found!",
+    });
+  }
+
+  const token = authorization.split(" ")[1];
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken; 
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).send({
+      message: "unauthorized access.",
+    });
+  }
+};
+
+
+
 
 async function run() {
   try {
@@ -48,25 +83,39 @@ async function run() {
       const result =await acceptCollection.find().toArray()
       res.send(result)
     })
-    app.get('/myAddedJob',async(req,res)=>{
-      const email = req.query.email
-      const result=await jobsCollection.find({userEmail:email}).toArray()
-      res.send(result)
-    })
+   app.get('/myAddedJob', verifyToken, async (req, res) => {
+      try {
+        const email = req.user.email;
+        const result = await jobsCollection.find({ userEmail: email }).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Failed to fetch jobs" });
+      }
+    });
 
 
 //  post data
-   app.post('/my-accepted-task', async(req,res)=>{
+   app.post('/my-accepted-task',verifyToken, async(req,res)=>{
   const data = req.body
+  data.acceptedBy = req.user.email; 
+  data.acceptedAt = new Date();
   const result = await acceptCollection.insertOne(data)
   res.send(result)
 })
-  app.post('/addJob',async(req,res)=>{
-   const newJobs =  req.body
-        const result= await jobsCollection.insertOne(newJobs)
-        res.send(result)
-  })
-
+  app.post('/addJob', verifyToken, async (req, res) => {
+      try {
+        const newJob = req.body;
+        newJob.userEmail = req.user.email;
+        newJob.postedBy = req.user.name || req.user.email;
+        newJob.postedAt = new Date();
+        const result = await jobsCollection.insertOne(newJob);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Failed to add job" });
+      }
+    });
 // delete
 app.delete('/my-accepted-task/:id',async(req,res)=>{
   const { id } = req.params;
